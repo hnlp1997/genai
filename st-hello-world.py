fb53f615411c45497e58cf860cba608903bd8940
Hoang-Nam LuuPham <hnluupham@gmail.com>
2024-04-30 18:51:26 -0700

    Update st-hello-world.py

--- st-hello-world.py
+++ st-hello-world.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
-st.write("Hello World")
+st.title("Steam Game Reviews Wizard :hamburger:")
+
+with st.form(key='my_form'):
+  text_input = st.text_input(label='Write a query asking about food in the Bay Area:')
+  submit_button = st.form_submit_button(label='Submit')
+
